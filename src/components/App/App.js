9d45fe8e579729cf4db67731a6fdf861a4e744fd
stivensson <stivensson.is@gmail.com>
2023-08-15 17:59:18 +0300

    fixed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
+import './App.css'
+
+import Header from '../Header'
+
 export default class App extends React.Component {
   render() {
-    return 123
+    return <Header />
   }
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Star DB</h1>
+      <ul>
+        <li>
+          <a href="#">People</a>
+        </li>
+        <li>
+          <a href="#">Planets</a>
+        </li>
+        <li>
+          <a href="#">Starships</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
